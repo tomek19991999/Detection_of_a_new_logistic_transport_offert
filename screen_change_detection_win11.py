302536ff372b27842cbb1f5394cc393c580cad21
tomek19991999 <51246267+tomek19991999@users.noreply.github.com>
2023-04-03 13:09:36 +0200

    Add files via upload

--- screen_change_detection_win11.py
+++ screen_change_detection_win11.py
@@ -1,69 +1,48 @@
-from pynput import keyboard
-from pynput import mouse #pip install pynput
 from PIL import ImageGrab #pip install pillow
-from pygame import mixer
+from pynput import mouse #pip install pynput
+import keyboard
 import time
-import screeninfo #pip install screeninfo
 import winsound
 
-def getHex(rgb):
-    return '%02X%02X%02X' % rgb
-
-def checkColor():
-    global prev_color, coords
-    # take info about monitor
-    monitors = screeninfo.get_monitors()
-    # select monitor
-    monitor = monitors[0]
-    # take coordinates of pixels
-    bbox = (monitor.x + coords[0], monitor.y + coords[1], monitor.x + coords[0] + 1, monitor.y + coords[1] + 1)
-    im = ImageGrab.grab(bbox=bbox)
-    rgbim = im.convert('RGB')
-    r, g, b = rgbim.getpixel((0, 0))
-    color = (r, g, b)
-    print(f'COLOR: rgb{(r,g,b)} | HEX #{getHex((r,g,b))}')
-    print(f'Coords: {coords}')
-    if prev_color is not None and color != prev_color:
-        #dzwiek
-        winsound.Beep(1000,300) #(Hz, glosnosc)
-
-    prev_color = color
-    time.sleep(1)
 
 
-def on_press(key):
-    global capturing, coords, stop
-    if key == keyboard.KeyCode(char='\\'):
-        capturing = True
-        print("Click on a pixel to capture its color")
-    elif key == keyboard.Key.esc:
-        capturing = False
-        stop = True
-        print("Exiting program")
-        return False
-    return True
+def on_click(x2, y2, button, pressed):
+    global x, y
+    if pressed:
+        print('Mouse clicked at ({0}, {1})'.format(x2, y2))
+        x=x2
+        y=y2
+        listener.stop()
 
-def on_click(x, y, button, pressed):
-    global capturing, coords, last_capture_time
-    if capturing and pressed and button == mouse.Button.left:
-        capturing = False
-        coords = (x, y)
-        last_capture_time = time.monotonic()
-        print(f"Captured color at {coords}")
+prev_x=0
+x=0
+prev_y=0
+y=0
+px = ImageGrab.grab().load()
+prev_color = px[prev_x, prev_y]
 
-capturing = False
-coords = None
-last_capture_time = 0
-prev_color = None
-count = 0
-stop = False
+while True:
+    if keyboard.is_pressed('esc'):
+        print("Exiting program!")
+        break
+    if keyboard.is_pressed('\\'):
+        print("clicked \!")
+        with mouse.Listener(on_click=on_click) as listener:
+            listener.join()
 
-if __name__ == '__main__':
-    with keyboard.Listener(on_press=on_press) as klstnr:
-        with mouse.Listener(on_click=on_click) as mlstnr:
-            while not stop:
-                if coords and time.monotonic() - last_capture_time > 1:
-                    checkColor()
-                    last_capture_time = time.monotonic()
-            klstnr.join()
-            mlstnr.join()
+    px = ImageGrab.grab().load()
+    color = px[x, y]
+    if color!=prev_color:
+        print("Color changed! R:",color[0],"G:",color[1],"B:",color[2])
+        #sound
+        winsound.Beep(1000,300) #(Hz, volume)
+
+    if x!=prev_x or y!=prev_y:
+        print("Changed coordinates to: x:",x," y:",y)
+
+    prev_color=color
+    prev_x=x
+    prev_y=y
+
+    time.sleep(0.01)
+
